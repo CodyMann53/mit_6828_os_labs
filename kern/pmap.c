7756b136029748b755e17cef1a63a5e83f4329ce
codymann <codymann@debianworkstation.hsd1.va.comcast.net>
2023-02-18 23:23:33 -0500

    Adding quick check to round up user_mem_check_addr for corner case.

--- kern/pmap.c
+++ kern/pmap.c
@@ -617,6 +617,11 @@ user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 		}
 	}
 
+	if (user_mem_check_addr < (uintptr_t) va)
+	{
+		user_mem_check_addr = (uintptr_t) va;
+	}
+
 	return 0;
 }
 
